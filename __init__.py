6e9e7fcb2968c2bab4453d002ebf5f9aa211f168
Martin Dobias <wonder.sk@gmail.com>
2008-11-14 11:20:11 +0000

    added qgisMinimumVersion() tag to all plugins, will be required by QGIS soon
    git-svn-id: svn+ssh://urtax.ms.mff.cuni.cz/home/projects/qgis-mapper/svn/trunk/sandbox/postgis_manager@113 b194d37e-d167-4ec0-bacc-68c1d414f356

--- __init__.py
+++ __init__.py
@@ -8,6 +8,9 @@ def description():
 def version():
 	return "Version 0.3"
 
+def qgisMinimumVersion():
+	return "1.0.0"
+
 def classFactory(iface):
 	from ManagerPlugin import ManagerPlugin
 	return ManagerPlugin(iface)
